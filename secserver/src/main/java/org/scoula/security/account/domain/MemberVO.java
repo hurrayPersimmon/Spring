34445bdc10d9c3ee29b29e2286eebebf6af70ce6
hurrayPersimmon <rahel337@naver.com>
2026-06-26 10:23:49 +0900

    Spring Security

---
+++ secserver/src/main/java/org/scoula/security/account/domain/MemberVO.java
@@ -0,0 +1,19 @@
+package org.scoula.security.account.domain;
+
+import java.util.Date;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberVO {
+    private String username;
+    private String password;
+    private String email;
+    private Date regDate;
+    private Date updateDate;
+    private List<AuthVO> authList; // 권한 목록, join 처리 필요
+}
